70f2c3122bb1585001a838762949f30255412da9
omerdr7 <omerfarukdere32@gmail.com>
2024-06-13 20:20:32 +0300

    Isperfect method created to check Is given number perfect number.

--- src/perfectnumbers/PerfectNumbers.java
+++ src/perfectnumbers/PerfectNumbers.java
@@ -1,12 +1,23 @@
 package perfectnumbers;
 
-
 public class PerfectNumbers {
 
-  
-    public static void main(String[] args) {
-       
-        
+    public static boolean isPerfect(int sayi) {
+        boolean control = false;
+        int sumOfFactors = 0;
+        for (int i = 1; i < sayi; i++) {
+            if (sayi % i == 0) {
+                sumOfFactors += i;
+            }
+        }
+        if (sumOfFactors == sayi) {
+            control = true;
+        }
+        return control;
     }
-    
+
+    public static void main(String[] args) {
+
+    }
+
 }
